936c8087dcbf76e3322d834e5c9f2be08c84062a
Patevansh <Vanshp9824@gmail.com>
2024-02-23 20:15:58 +0530

    app

--- App1/ven/Scripts/activate.bat
+++ App1/ven/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\Python\Python\Web_python\ven
+set VIRTUAL_ENV=E:\Python\Python\App1\ven
 
 if not defined PROMPT set PROMPT=$P$G
 
