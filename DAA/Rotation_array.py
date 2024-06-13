66cf0ce17391053a3d6f4fae566551ce72a38a04
Patevansh <Vanshp9824@gmail.com>
2024-06-13 13:36:48 +0530

    ro

--- DAA/Rotation_array.py
+++ DAA/Rotation_array.py
@@ -1,6 +1,14 @@
 arr=[1,2,3,4,5,6,7]
 r=int(input("Enter a number:"))
+l=len(arr)
+# for i in range(r):
+#     arr.append(arr[0])
+#     arr.remove(arr[0])
+# print(arr)
+
 for i in range(r):
-    arr.append(arr[0])
-    arr.remove(arr[0])
+    for j in range(l-1):
+        temp=arr[j]
+        arr[j]=arr[j+1]
+        arr[j+1]=temp
 print(arr)
